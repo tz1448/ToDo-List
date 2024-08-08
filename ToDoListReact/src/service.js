ad4ad2b3600092a875e0bf88643bc7a87e30e14b
Tzofia Ben-Zion <137272212+tz1448@users.noreply.github.com>
2024-08-08 14:08:16 +0300

    Update service.js

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API;
+axios.defaults.baseURL  = process.env.REACT_APP_API_URL;;
 
 axios.interceptors.response.use(function (response) {
   console.log(`intercaption ${response.statusText}`);
